e849929cbecb3d61bd5d6e5665dca82899b54c7b
Bladesheng <keadr23@gmail.com>
2025-05-01 15:30:51 +0200

    hide image groups without images

--- resources/js/components/ImageGallery.vue
+++ resources/js/components/ImageGallery.vue
@@ -1,36 +1,43 @@
 <script setup lang="ts">
 import { Images } from 'tmdb-ts';
+import { computed } from 'vue';
 
 const { images } = defineProps<{
 	images: Omit<Images, 'id'>;
 }>();
+
+const groupsWithImages = computed(() =>
+	Object.fromEntries(Object.entries(images).filter(([groupName, group]) => group.length))
+);
 </script>
 
 <template>
 	<div class="tabs tabs-lift">
-		<template v-for="([groupName, group], i) in Object.entries(images)" :key="groupName">
-			<input
-				type="radio"
-				class="tab capitalize"
-				name="imageGallery"
-				:aria-label="`${groupName} (${group.length})`"
-				:checked="i === 0"
-			/>
+		<template v-for="([groupName, group], i) in Object.entries(groupsWithImages)" :key="groupName">
+			<template v-if="group.length">
+				<input
+					type="radio"
+					class="tab capitalize"
+					name="imageGallery"
+					:aria-label="`${groupName} (${group.length})`"
+					:checked="i === 0"
+				/>
 
-			<div class="tab-content border-base-300 p-6">
-				<div class="flex flex-col gap-4">
-					<img
-						v-for="image in group"
-						:key="image.file_path"
-						loading="lazy"
-						:src="`https://image.tmdb.org/t/p/w1280${image.file_path}`"
-						width="1280"
-						:height="1280 / image.aspect_ratio"
-						alt=""
-						class="rounded"
-					/>
+				<div class="tab-content border-base-300 p-6">
+					<div class="flex flex-col gap-4">
+						<img
+							v-for="image in group"
+							:key="image.file_path"
+							loading="lazy"
+							:src="`https://image.tmdb.org/t/p/w1280${image.file_path}`"
+							width="1280"
+							:height="1280 / image.aspect_ratio"
+							alt=""
+							class="rounded"
+						/>
+					</div>
 				</div>
-			</div>
+			</template>
 		</template>
 	</div>
 </template>
