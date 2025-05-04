17cb9fe7598ed8e6316332cfd293776c5da6b277
Bladesheng <keadr23@gmail.com>
2025-05-04 12:26:49 +0200

    add meta description

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta
+			name="description"
+			content="Discover new and upcoming movies and TV shows. Stay up to date with what's airing now and what's coming soon."
+		/>
+
 		@vite('resources/css/app.css')
 		@vite('resources/js/app.ts')
+
 		@inertiaHead
 		@routes
 	</head>
