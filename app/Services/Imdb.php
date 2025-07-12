c6300b2d8b776da8c819744087a51609d5bea708
Bladesheng <keadr23@gmail.com>
2025-07-12 16:41:27 +0200

    fix 404 response from imdb

--- app/Services/Imdb.php
+++ app/Services/Imdb.php
@@ -26,13 +26,16 @@ class Imdb
 			return $details;
 		}
 
-		$body = Http::withHeaders([
+		$response = Http::withHeaders([
 			'User-Agent' =>
 				'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
-		])
-			->get("https://www.imdb.com/title/{$id}")
-			->throw()
-			->body();
+		])->get("https://www.imdb.com/title/{$id}");
+
+		if ($response->status() === 404) {
+			return null;
+		}
+
+		$body = $response->throw()->body();
 
 		$crawler = new Crawler($body);
 		$ratingContainer = $crawler->filter('[data-testid="rating-histogram"]');
